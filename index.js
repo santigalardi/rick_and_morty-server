edcef1ee0318e3e5ac85b7c189b4d3896fab3f48
Santiago Galardi <santygalardi@gmail.com>
2023-07-30 19:33:29 -0300

    vercel update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-const router = require('./routes/index');
+const router = require('./src/routes/index');
 const PORT = 3001;
 
 app.use(express.json());
